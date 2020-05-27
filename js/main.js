133734e582a362a45d07ff714aabda5e6e2f2f32
chamd <50565709+chamd@users.noreply.github.com>
2020-05-27 15:28:18 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,7 @@
-function showMenu() {
-    alert("아직 구현되지 않은 기능입니다!");
-    answer = prompt("참드홈이 마음에 드십니까? [Y/N]", "");
-    if(answer=="Y"){
-        alert("감사합니다!");
-    }else if(answer=="N"){
-        alert("ㅅ1발 어쩌라고;;");
-    }else{
-        alert("죄송하지만, 못 알아 듣겠습니다.")
-    }
+function openMenu() {
+    document.getElementById("Menubar").style.width = "400px";
+}
+
+function closeMenu() {
+    document.getElementById("Menubar").style.width = "0";
 }
